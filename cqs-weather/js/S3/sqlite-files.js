19691759ed51021eb0af90a2fcf7fb19aecc93b0
eduardfofe <eduardfores@gmail.com>
2022-04-05 21:40:18 +0200

    Main: add S3 connections with worker sqlite

--- cqs-weather/js/S3/sqlite-files.js
+++ cqs-weather/js/S3/sqlite-files.js
@@ -4,6 +4,22 @@
  * @returns data object (json) with posts from S3
  */
  const getDBs = () => {
-    var response = s3.listObjectsV2({ Prefix: 'databases/' }).promise();
-    return response;
+    return s3.listObjectsV2({ Prefix: 'databases/database' }).promise();
+}
+
+function getSQLiteToday() {
+    var date = getDate();
+    var params = {
+        Key : "databases/database"+date[0]+"-"+date[1]+"-"+date[2]+".db"
+    }
+    return s3.getObject(params).promise();
+}
+
+function getDate() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    return [dd, mm, yyyy];
 }
---
+++ cqs-weather/js/sqlite/sqlite-worker.js
@@ -0,0 +1,16 @@
+const createWorker = () => {
+    // Start the worker in which sql.js will run
+    var worker = new Worker("../dist/worker.sql-wasm.js");
+    worker.onerror = error;
+
+    // Open a database
+    worker.postMessage({ action: 'open' });
+
+    return worker;
+}
+
+function error(e) {
+	console.log(e);
+	errorElm.style.height = '2em';
+	errorElm.textContent = e.message;
+}
